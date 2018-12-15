db9e057a342f1f45382a611fec0e5cf23ec460a7
clauszitzelsberger <40275652+clauszitzelsberger@users.noreply.github.com>
2018-12-15 16:06:00 +0100

    set hidden layers variable

--- model.py
+++ model.py
@@ -1,35 +1,36 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from collections import OrderedDict
+#from collections import OrderedDict
 
 class QNetwork(nn.Module):
     """Deep Q Network """
 
     def __init__(self, state_size,
-                 action_size, fc1=256,
-                 fc2=128, fc3=64,
+                 action_size, hidden_layers=[256, 128, 64],
                  seed=1):
         """Initialize paramteres and build model
         Params
         ======
             state_size (int): Dimension of state
             action_size (int): Dimension of action
-            fc1, fc2, fc3 (int): Number of nodes for each hidden layer
+            hidden_layers: list of integers, the sizes of the hidden layers
             seed (int): random seed
         """
-        super(QNetwork, self).__init__()
-
+        super().__init__()
         self.seed = torch.manual_seed(seed)
-        self.model = nn.Sequential(OrderedDict([
-                  ('fc1', nn.Linear(state_size, fc1)),
-                  ('relu1', nn.ReLU()),
-                  ('fc2', nn.Linear(fc1, fc2)),
-                  ('relu2', nn.ReLU()),
-                  ('fc3', nn.Linear(fc2, fc3)),
-                  ('relu3', nn.ReLU()),
-                  ('output', nn.Linear(fc3, action_size))]))
+
+        # Input layer to hidden layer
+        self.hidden_layers = \
+            nn.ModuleList([nn.Linear(state_size, hidden_layers[0])])
+
+        # Add a variable number of hidden hidden_layers
+        layer_sizes = zip(hidden_layers[:-1], hidden_layers[1:])
+        self.hidden_layers.extend([nn.Linear(h1, h2) for h1, h2 in layer_sizes])
+        self.output = nn.Linear(hidden_layers[-1], action_size)
 
     def forward(self, state):
         """Build a network that maps state to action values"""
-        return self.model.forward(state)
+        for each in self.hidden_layers:
+            state = F.relu(each(state))
+        return self.output(state)
